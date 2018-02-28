e55be2064ebe145216e4dbce2e0547ffbd17af53
Rahul Choudhary <choudharyr@gmail.com>
2018-02-28 14:22:43 +0530

    algolia places implementation

---
+++ AlgoliaWeb/js/places.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    var placesAutocomplete = places({
+        container: document.querySelector('#form-address'),
+        type: 'address',
+        templates: {
+            value: function (suggestion) {
+                return suggestion.name;
+            }
+        }
+    });
+    placesAutocomplete.on('change', function resultSelected(e) {
+        console.log(e);
+        document.querySelector('#form-address2').value = e.suggestion.administrative || '';
+        document.querySelector('#form-city').value = e.suggestion.city || '';
+        document.querySelector('#form-zip').value = e.suggestion.postcode || '';
+        document.querySelector('#form-country').value = e.suggestion.country || '';
+    });
+})();
