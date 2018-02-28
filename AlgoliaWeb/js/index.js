7b1a30c15d72c0ab4ae8c245bbea8d9973d9ac38
Rahul Choudhary <choudharyr@gmail.com>
2018-02-28 14:22:28 +0530

    facet search

---
+++ AlgoliaWeb/js/index.js
@@ -0,0 +1,106 @@
+﻿// Algolia client. Mandatory to instantiate the Helper.
+var algolia = algoliasearch('CDP0NI9PBE', '833fd7607856c809f180787e17124438');
+
+// Algolia Helper
+var helper = algoliasearchHelper(algolia, 'Blogs', {
+    // Facets need to be explicitly defined here
+    facets: ['Blog.Name', 'Blog.BlogPost.email'],
+    // Misc. configuration for the demo purpose 
+    hitsPerPage: 5,
+    maxValuesPerFacet: 5
+});
+
+// Bind the result event to a function that will update the results
+helper.on("result", searchCallback);
+
+// The different parts of the UI that we want to use in this example
+var $hits = $('#hits');
+var $facets = $('#facets');
+
+$facets.on('click', handleFacetClick);
+
+// Trigger a first search, so that we have a page with results
+// from the start.
+helper.search();
+
+// Result event callback
+function searchCallback(results) {
+    if (results.hits.length === 0) {
+        // If there is no result we display a friendly message
+        // instead of an empty page.
+        $hits.empty().html("No results :(");
+        return;
+    }
+
+    // Hits/results rendering
+    renderHits($hits, results);
+    renderFacets($facets, results);
+}
+
+function renderHits($hits, results) {
+    // Scan all hits and display them
+    var hits = results.hits.map(function renderHit(hit) {
+        // We rely on the highlighted attributes to know which attribute to display
+        // This way our end-user will know where the results come from
+        // This is configured in our index settings
+        var highlighted = hit._highlightResult;
+        var attributes = $.map(highlighted, function renderAttributes(attribute, name) {
+            console.log(attribute);
+
+            /*
+             
+
+             <div class="hit" onclick="getDetails(&quot;3813222&quot;)"><div class="attribute"><span>Blog: </span><em>C</em>onsumerSales - <span>Title: </span>Rapdudor  <em>C</em>ompany - <span>CommentText: </span>efficitur nec. nibh. <em>c</em>onsequat odio
+
+Nam et - <span>CommentByUser: </span>dougcatcher - <span>CommentDate: </span>2017-02-14T17:16:55.78 - <span>UnixTime: </span>1487010600</div></div>
+
+             */
+            var html = '<div class="attribute">' +
+                '<span>' + name + ': </span>' +
+                attribute.Name.value;
+
+            if (attribute.BlogPost.title.matchedWords.length > 0) {
+                html += ' - <span>Title: </span>' + attribute.BlogPost.title.value;
+            }
+
+            var comment = attribute.BlogPost.Comment;
+            if (comment.text.matchedWords.length > 0 || comment.username.matchedWords.length > 0 || comment.dateposted.matchedWords.length > 0) {
+                html += ' - <span>CommentText: </span>' + attribute.BlogPost.Comment.text.value;
+                html += ' - <span>CommentByUser: </span>' + attribute.BlogPost.Comment.username.value;
+                html += ' - <span>CommentDate: </span>' + attribute.BlogPost.Comment.dateposted.value;
+                html += ' - <span>UnixTime: </span>' + hit.Blog.BlogPost.Comment.DatePostedUnixTimestamp;
+            }
+
+            html += '</div>';
+
+            return html;
+        }).join('');
+        return '<div class="hit panel" onclick=getDetails("' + hit.objectID + '")>' + attributes + '</div>';
+    });
+    $hits.html(hits);
+}
+
+function renderFacets($facets, results) {
+    var facets = results.facets.map(function (facet) {
+        var name = facet.name;
+        var header = '<h4>' + name + '</h4>';
+        var facetValues = results.getFacetValues(name);
+        var facetsValuesList = $.map(facetValues, function (facetValue) {
+            var facetValueClass = facetValue.isRefined ? 'refined' : '';
+            var valueAndCount = '<a data-attribute="' + name + '" data-value="' + facetValue.name + '" href="#">' + facetValue.name + ' (' + facetValue.count + ')' + '</a>';
+            return '<li class="' + facetValueClass + '">' + valueAndCount + '</li>';
+        })
+        return header + '<ul>' + facetsValuesList.join('') + '</ul>';
+    });
+
+    $facets.html(facets.join(''));
+}
+
+function handleFacetClick(e) {
+    e.preventDefault();
+    var target = e.target;
+    var attribute = target.dataset.attribute;
+    var value = target.dataset.value;
+    if (!attribute || !value) return;
+    helper.toggleRefine(attribute, value).search();
+}
---
+++ AlgoliaWeb/js/index2.js
@@ -0,0 +1,133 @@
+﻿/**
+         * This example uses the raw JavaScript client to build an instant search result page.
+         * If you plan on building such a UI, we strongly encourage you to try our instantsearch.js library instead.
+         * It is a set of UI widgets that you can mix and match to build instant search result pages, including facet filtering,
+         * highlighting and custom themes.
+         * More information, examples and documentation on https://community.algolia.com/instantsearch.js/
+        **/
+
+function searchCallback(err, content) {
+    if (err) {
+        // error
+        return;
+    }
+
+    if (content.query !== $('#q').val()) {
+        // do not take out-dated answers into account
+        return;
+    }
+
+    if (content.hits.length === 0) {
+        // no results
+        $('#hits').empty();
+        return;
+    }
+
+    // Scan all hits and display them
+    var html = '';
+    for (var i = 0; i < content.hits.length; ++i) {
+        var hit = content.hits[i];
+        //console.log(hit);
+        html += '<div class="hit" onclick=getDetails("' + hit.objectID + '")>';
+        for (var attribute in hit._highlightResult) {
+            //console.log("attribute: " + attribute);
+            //console.log(hit._highlightResult[attribute]);
+            //console.log(hit._highlightResult[attribute].Name);
+            html += '<div class="attribute">' +
+                '<span>' + attribute + ': </span>' +
+                hit._highlightResult[attribute].Name.value;
+
+            if (hit._highlightResult[attribute].BlogPost.title.matchedWords.length > 0) {
+                html += ' - <span>Title: </span>' + hit._highlightResult[attribute].BlogPost.title.value;
+            }
+
+            var comment = hit._highlightResult[attribute].BlogPost.Comment;
+            if (comment.text.matchedWords.length > 0 || comment.username.matchedWords.length > 0 || comment.dateposted.matchedWords.length > 0) {
+                html += ' - <span>CommentText: </span>' + hit._highlightResult[attribute].BlogPost.Comment.text.value;
+                html += ' - <span>CommentByUser: </span>' + hit._highlightResult[attribute].BlogPost.Comment.username.value;
+                html += ' - <span>CommentDate: </span>' + hit._highlightResult[attribute].BlogPost.Comment.dateposted.value;
+                html += ' - <span>UnixTime: </span>' + hit.Blog.BlogPost.Comment.DatePostedUnixTimestamp;
+            }
+
+            html += '</div>';
+        }
+        html += '</div>';
+    }
+    $('#hits').html(html);
+}
+
+function addFacetItem(elemId, facetValue) {
+    var ul = document.getElementById(elemId); //ul
+    var li = document.createElement('li');//li
+
+    var checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    checkbox.value = facetValue;
+
+    li.appendChild(checkbox);
+
+    li.appendChild(document.createTextNode(facetValue));
+    ul.appendChild(li);
+}
+
+function getFacetValues(err, content) {
+    if (err == null) {
+        var json = content.facets;
+        var names = json["Blog.Name"];
+        for (var n in names) {
+            if (names.hasOwnProperty(n)) {
+                var txt = n + " (" + names[n] + ")";
+                //addFacetItem("ulEmails", txt);
+            }
+        }
+    }
+}
+
+var index;
+var $inputfield;
+$(document).ready(function () {
+    $inputfield = $('#q');
+
+    // Replace the following values by your ApplicationID and ApiKey.
+    var client = algoliasearch('CDP0NI9PBE', '833fd7607856c809f180787e17124438');
+    // Replace the following value by the name of the index you want to query.
+    index = client.initIndex('Blogs');
+
+    index.search('', { facets: '*', hitsPerPage: 0 }, getFacetValues);
+
+    $inputfield.keyup(function () {
+        if ($.trim($inputfield.val()).length > 0) {
+            var filter = "";
+            if ($("#dt").val()) {
+                var unixDt = moment($("#dt").val()).unix();
+                filter = "Blog.BlogPost.Comment.DatePostedUnixTimestamp = " + unixDt;
+            }
+            index.search({
+                query: $inputfield.val(),
+                facets: ['*'],
+                filters: filter
+            }, searchCallback);
+        }
+    }).focus().closest('form').on('submit', function () {
+        // on form submit, store the query string in the anchor
+        location.replace('#q=' + encodeURIComponent($inputfield.val()));
+        return false;
+    });
+
+    // check if there is a query in the anchor: http://example.org/#q=my+query
+    if (location.hash && location.hash.indexOf('#q=') === 0) {
+        console.log(location.hash);
+        var q = decodeURIComponent(location.hash.substring(3));
+        $inputfield.val(q).trigger('keyup');
+    }
+});
+
+function getDetails(objectId) {
+    index.getObject(objectId, function (err, content) {
+        console.log(content);
+    });
+}
+
+$("#btnSearch").on("click", function () {
+    $inputfield.trigger('keyup');
+});
